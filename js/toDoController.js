f043cdbc456e9c09756a4e9172c704126357cdcc
Ajitsy <sanghera.p@gmail.com>
2015-09-14 08:58:26 +0100

    added clear completed and show active tasks

--- js/toDoController.js
+++ js/toDoController.js
@@ -3,7 +3,23 @@ ToDoList.controller('ToDoController', ['$scope', function($scope) {
   $scope.todos = [];
 
   $scope.submitTodo = function(){
-    $scope.todos.push({'title':$scope.newTodo});
+    $scope.todos.push({'title':$scope.newTodo, 'complete':false});
+    $scope.newTodo = '';
   };
+  $scope.clearCompletedTasks = function(){
+    $scope.todos = $scope.todos.filter(function(task){
+      return !task.complete;
+    });
+  };
+  $scope.completedTasks = function(){
+    $scope.todos = $scope.todos.filter(function(task){
+      return task.complete;
+    });
+  };
+  $scope.allTasks = function(){
+    return $scope.todos;
+
+  };
+
 
 }]);
--- test/e2e/toDoFeature.js
+++ test/e2e/toDoFeature.js
@@ -1,7 +1,6 @@
 describe('ToDo List', function() {
 
 var todos = element.all(by.repeater('todo in todos'));
-
 var inputBox = element(by.model('newTodo'));
 var createButton = element(by.className('btn'));
 
@@ -12,18 +11,21 @@ var createButton = element(by.className('btn'));
 
   it('should add a todo item', function() {
     expect(todos.count()).toBe(0);
-    inputBox.sendKeys('task 2');
+    inputBox.sendKeys('task 1');
     element(by.css('button')).click();
     expect(todos.count()).toBe(1);
-    expect(todos.last().getText()).toEqual('task 2');
+    expect(todos.last().getText()).toEqual('task 1');
   });
 
   it('should mark item as completed', function() {
-    inputBox.sendKeys('task 2');
-    element(by.css('button')).click();
     expect(element(by.model('todo.complete')).isSelected()).toBeFalsy();
     element(by.model("todo.complete")).click();
     expect(element(by.model('todo.complete')).isSelected()).toBeTruthy();
+  });
+
+  it('clear completed tasks', function() {
+    element(by.buttonText("Clear Completed Tasks"));
+    expect(todos.count()).toBe(1);
   });
 
 });
