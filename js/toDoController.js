0157ce8ff1af946bafcc387c10b921f57f11293f
Ajitsy <sanghera.p@gmail.com>
2015-10-06 17:14:16 +0100

    .todos =, is no longer rewriting the view model everytime with permanent changes when tasks are cleared

--- js/toDoController.js
+++ js/toDoController.js
@@ -1,25 +1,29 @@
 ToDoList.controller('ToDoController', ['$scope', function($scope) {
 
   $scope.todos = [];
+  todosvar = [];
 
   $scope.submitTodo = function(){
     $scope.todos.push({'title':$scope.newTodo, 'complete':false});
+    todosvar.push({'title':$scope.newTodo, 'complete':false});
     $scope.newTodo = '';
   };
+
   $scope.clearCompletedTasks = function(){
+    $scope.todos = todosvar;
     $scope.todos = $scope.todos.filter(function(task){
       return !task.complete;
     });
   };
   $scope.completedTasks = function(){
+    $scope.todos = todosvar;
     $scope.todos = $scope.todos.filter(function(task){
       return task.complete;
     });
   };
   $scope.allTasks = function(){
+    $scope.todos = todosvar;
     return $scope.todos;
-
   };
-
 
 }]);
